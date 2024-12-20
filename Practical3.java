06836a4d27574d3583498ab04af98ea4d5c659be
Ramya-Vi <ramyavishwanathan11@gmail.com>
2024-12-20 04:43:41 +0000

    Practical3

--- Practical3.java
+++ Practical3.java
@@ -42,7 +42,7 @@ class Stack1
             return stck[top];
         }
     }
-}
+}                                                                                                                                                                                             
 public class Practical3
 {
     public static void main(String args[])
